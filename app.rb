18a24089c37cf3adcceeaca28fafc8f86d766413
GabbyMont <gemo1236@gmail.com>
2018-04-03 13:10:16 -0400

    cleaned up

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require_relative 'pairing_function.rb'
 
 enable :sessions
-# def random_array(names)
+# def random_array(names) //// This is my pairing function(just here for reference)
 # 	random_name_array = names.shuffle.each_slice(2).to_a
 # 	if names.size % 2 == 0
 # 		random_name_array
@@ -15,67 +15,53 @@ enable :sessions
 # end
 
 get '/' do
-	erb :give_name1
+	erb :give_name1 #transferring to 'give_name1' erb page
 end
 
 post '/username' do
-	username = params[:username]
+	username = params[:username] # assigning params[:username] to 'username'
 	# "First name here #{username}" => First name here 'input name'
-	redirect '/getrandomnames?username=' + username
+	redirect '/getrandomnames?username=' + username #redirects to getrandomnames get and adding username
 end
 
 get '/getrandomnames' do
-	username = params[:username]
+	username = params[:username] # continue 
 	erb :get_random_names2, locals:{username:username}
 end
 
 post '/getrandomnames' do
-	name1 = params[:name1]
-	name2 = params[:name2]
-	name3 = params[:name3]
-	name4 = params[:name4]
-	name5 = params[:name5]
-	name6 = params[:name6]
+	array = params[:new_name]
 	username = params[:username]
-	array = [name1,name2,name3,name4,name5,name6]
 	session[:name_array] = random_array(array)
 	session[:liked_pairs] = []
-	# "Your names are #{name1}, #{name2}, #{name3}, #{name4}, #{name5}, #{name6}"
-	# p array
-	# p array.class
-	# p array.length
-	# p "#{name_array}"
 	redirect '/check?username=' + username
 end
 
 get '/check' do
 	username = params[:username]
-	erb :check_names, locals:{username:username}
+	erb :check_names, locals:{username:username, pairs_array:session[:name_array]}
 end
 
 post '/check_names' do
-	username = params[:username]
-	session[:pairs] = params[:teams]
-	session[:pairs] = session[:pairs]
+	username = params[:username] #continuing use of username params(instead of session)
+	session[:pairs] = params[:teams] 
 
 	temp_array = []
 	session[:name_array].each do |name|
 		temp_array.push(name.join(','))
 	end
-	session[:name_array] = temp_array
-
-
-	leftovers = []
-	session[:name_array].each do |pairs|
-		if session[:pairs].include?(pairs) == false
-			leftovers << pairs.split(',')
+	session[:name_array] = temp_array # Setting name_array session to the empty array
+	leftovers = [] # Creating a new aray for left over pairs or pairs not selected by the user
+	session[:name_array].each do |pairs| # Loop
+		if session[:pairs].include?(pairs) == false # if the pairs do not match they are pushed into the leftovers array
+			leftovers << pairs.split(',') 
 		else
 			session[:liked_pairs] << pairs.split(',')
 		end
 	end
 
 	session[:leftovers] = leftovers.flatten
-	if session[:leftovers].length == 0
+	if session[:leftovers].length == 0 
 		redirect '/final_result?username=' + username
 	else
 		session[:name_array] = random_array(session[:leftovers])
@@ -89,6 +75,6 @@ get '/final_result' do
 end 
 
 post '/start_over_button' do
-	redirect '/'
+	redirect '/getrandomnames'
 end
 
